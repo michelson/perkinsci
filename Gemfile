93545c8472f82137d8ab4c336784fbdadef65a52
Miguel Michelson <miguelmichelson@gmail.com>
2016-02-11 22:17:51 -0300

    mini card & nprogress

--- Gemfile
+++ Gemfile
@@ -17,6 +17,7 @@ gem 'chronic_duration'
 
 # Use jquery as the JavaScript library
 gem 'jquery-rails'
+gem 'nprogress-rails'
 # Turbolinks makes following links in your web application faster. Read more: https://github.com/rails/turbolinks
 gem 'turbolinks'
 # Build JSON APIs with ease. Read more: https://github.com/rails/jbuilder
--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,9 +10,13 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
+
+
 //= require jquery
 //= require jquery_ujs
 //= require turbolinks
+//= require nprogress
+//= require nprogress-turbolinks
 
 //= require vendor/ansiparse
 //= require vendor/minispade
