93a6bb59f03c9ddaee17bc453dd09c4fa225272e
Miguel Michelson <miguelmichelson@gmail.com>
2016-02-29 16:20:59 -0300

    fix job queuer query

--- app/jobs/job_queuer.rb
+++ app/jobs/job_queuer.rb
@@ -8,9 +8,9 @@ class JobQueuer
     Repo.all.find_each do |repo|
       collection = repo.build_reports
       # find if there is an enqueued
-      if report = collection.where(build_state: "queued").order("created_at DESC").last
+      if report = collection.where(build_status: "queued").order("created_at DESC").last
         # start job only if there are not started build for this repo
-        report.start! unless collection.where(build_state: "started").any?
+        report.start! unless collection.where(build_status: "started").any?
       end
     end
 
