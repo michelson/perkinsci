a2e7bff1b719ce9b99ba9a9ea0bf481f9db257d8
Boris Quiroz <boris@insert-coin.org>
2016-03-09 11:12:35 -0300

    add names

--- go-docker.sh
+++ go-docker.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-docker run -d -p 3306:3306 boris/perkinsci-db:1.0
-docker run -tiP -p 3000:3000 -v `pwd`:/app boris/perkinsci-dev:1.1 /bin/bash
+docker run -d -p 3306:3306 --name perkinsci-db boris/perkinsci-db:1.0
+docker run -tiP -p 3000:3000 --name perkinsci-dev -v `pwd`:/app boris/perkinsci-dev:1.1 /bin/bash
