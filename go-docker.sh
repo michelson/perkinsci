e7a8a5f74b7d457ffb316e08c01748facc686474
Boris Quiroz <boris@insert-coin.org>
2016-03-09 17:26:28 -0300

    add redis

--- go-docker.sh
+++ go-docker.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 docker run -d -p 3306:3306 --name perkinsci-db boris/perkinsci-db:1.0
+docker run -d -p 6379:6379 --name perkinsci-redis boris/redis
 docker run -tiP -p 3000:3000 --name perkinsci-dev -v `pwd`:/app boris/perkinsci-dev:1.1 /bin/bash
